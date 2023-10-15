4f76bc1ef2e520de506e220a068d923366dc097b
im-shikha <shikharajput1224@gmail.com>
2023-10-15 22:26:36 +0530

    ep-3-laying-the-foundation

--- App.js
+++ App.js
@@ -1,32 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/* Nested Structure in React
-<div id="parent">
-    <div id="child">
-          <h1></h1>
-          <h2></h2>
+//JSX is not HTML in JS. It is HTML-like syntax extension for JS.
+const Title = () => <h1 id="heading">Namaste React using JSX.</h1>;
+console.log(Title);
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      {Title()}
+      <Title />
+      <Title />
+      <Title></Title>
+      <h1 className="heading" tabIndex="5">
+        Namaste React from Functional Component!
+      </h1>
     </div>
-    <div id="child2">
-          <h1></h1>
-          <h2></h2>
-    </div>      
-</div>
-*/
-
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a heading1 inside nested structure."),
-    React.createElement("h2", {}, "I am a h2 inside nested structure."),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am  heading1 inside nested structure."),
-    React.createElement("h2", {}, "I am a h2 inside nested structure."),
-  ]),
-]);
-
-console.log(heading);
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
